e939147c0fdd85a83c53a916377a7419ee7ee2e0
Dmitriy <xs_ds@inbox.ru>
2025-12-17 13:14:22 +0500

    del middleware and fix settings

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'config.middleware.FixHostMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
